7923c29a7fa7db027e95df90b0fea4af3914fc7b
Comingdonut <jamescastrejon12@gmail.com>
2016-08-04 22:48:53 -0600

    Refactor Part 1

--- Chess/Chess/Chess/ChessView/Print.cs
+++ Chess/Chess/Chess/ChessView/Print.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Chess.ChessModels;
+using System;
 
 namespace Chess.ChessView
 {
@@ -20,17 +17,48 @@ namespace Chess.ChessView
         /// Prints out 24 array values before moving on to the next set of 24 values.
         /// </summary>
         /// <param name="boardSqaures">Represents a board. Required size for the 2-D array is 8 rows and 24 columns.</param>
-        public void PrintBoard(string[,] boardSqaures)
+        public void PrintBoard(ChessPiece[,] boardSqaures)
         {
-
             for (int x = 0; x < 8; ++x)
             {
-                for (int y = 0; y < 24; y++)
+                for (int y = 0; y < 8; y++)
                 {
-                    Console.Write(boardSqaures[x, y]);
+                    if (x % 2 == 0)
+                    {
+                        if (y % 2 == 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        }
+                    }
+                    else
+                    {
+                        if (y % 2 == 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                        }
+                    }
+                    if (boardSqaures[x, y].Color == 'l')
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if(boardSqaures[x, y].Color == 'd')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    Console.Write(" " + boardSqaures[x, y].Symbol + " ");
                 }
                 Console.WriteLine();
             }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
     //-----------------------------------------------------------------------------------
