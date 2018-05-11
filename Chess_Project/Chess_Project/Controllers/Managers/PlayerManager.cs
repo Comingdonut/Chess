7d7251362b65b99edafab40cfb473ce5925c125e
James Castrejon <jamescastrejon12@gmail.com>
2018-05-11 12:56:02 -0600

    Created player manager and player class.

---
+++ Chess_Project/Chess_Project/Controllers/Managers/PlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using Chess_Project.Models.Helper;
+using Chess_Project.Models.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Project.Controllers.Managers
+{
+    internal class PlayerManager
+    {
+        internal static PlayerManager pManager;
+        internal Player Player1 { get; set; }
+        internal Player Player2 { get; set; }
+        internal PlayerManager()
+        {
+            Player1 = new Player("Player 1", Color.white);
+            Player2 = new Player("Player 2", Color.black);
+        }
+        internal static PlayerManager GetInstance()
+        {
+            if(pManager == null)
+            {
+                pManager = new PlayerManager();
+            }
+            return pManager;
+        }
+        // Set Player Name
+        // Set Player Color
+        internal Player SwitchPlayer(Player p)
+        {
+            Player currPlayer;
+            if(p.Name == Player1.Name)
+                currPlayer = Player2;
+            else
+                currPlayer = Player1;
+            return currPlayer;
+        }
+    }
+}
---
+++ Chess_Project/Chess_Project/Models/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using Chess_Project.Models.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Project.Models.Player
+{
+    internal class Player
+    {
+        internal string Name { get; set; }
+        internal Color Color { get; set; }
+        // TODO: Time took
+        // TODO: History
+        internal Player() { }
+        internal Player(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
